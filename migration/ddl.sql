becc218d4fcda470ace30a03c2c0e6253041301b
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2024-01-18 12:11:39 +0200

    Update ddl.sql

--- migration/ddl.sql
+++ migration/ddl.sql
@@ -1,9 +1,10 @@
-CREATE TABLE Employee (
+CREATE TABLE members (
     id int PRIMARY KEY,
     first_name varchar(50),
     last_name varchar(50),
+    phone varchar(50),
     email varchar(50) UNIQUE NOT NULL,
-    phone bigint
+    password varchar (50) UNIQUE NOT NULL
 );
 
 
